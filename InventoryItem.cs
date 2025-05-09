b10b17cd245500915bb97e4ffdfe277cbc603468
Kris <Kris@email.com>
2025-05-09 23:35:57 +0300

    Создание класса InventoryItem

---
+++ InventoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    internal class InventoryItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        public InventoryItem(string name, int quantity, decimal price, string category)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Category = category;
+        }
+    }
+}
