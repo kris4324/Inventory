6d82f4b6fe942d371a74644f3e3a5a34ee5b7047
Kris <Kris@email.com>
2025-05-10 15:35:30 +0300

    Добавление класса InventoryManager и переименование формы в InventoryForm

---
+++ InventoryForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Inventory
+{
+    partial class InventoryForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // InventoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "InventoryForm";
+            this.Text = "InventoryForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
